627643945f9c67c84918ca66d71cea52df27af87
DannyTZ233 <70179486+DannyTZ233@users.noreply.github.com>
2022-03-25 04:30:03 -0500

    Add files via upload

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from flask_restful import Api
-
+from flask_sqlalchemy import SQLAlchemy
 from db import db
-from resources import *
+from ma import ma
+from resources.store import Store, StoreList
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/CIS4375'
-# data migration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
 
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    print('tables created')
+
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
+
 if __name__ == '__main__':
     db.init_app(app)
+    ma.init_app(app)
     app.run(port=5000, debug=True)
