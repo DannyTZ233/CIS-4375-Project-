be21234fa4cb658d145b3f4c08ff00cba68218fd
DannyTZ233 <70179486+DannyTZ233@users.noreply.github.com>
2022-03-25 04:28:00 -0500

    Add files via upload

---
+++ Backend/models/store.py
@@ -0,0 +1,28 @@
+from db import db
+
+
+class StoreModel(db.Model):
+    __tablename__ = 'stores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    address = db.Column(db.String(100))
+
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+    # def json(self):
+    #     return {'name': self.name, 'address': self.address}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
