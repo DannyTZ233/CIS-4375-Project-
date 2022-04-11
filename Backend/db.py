4f900d148286735ba82935867fe5530fdf01e6a8
DannyTZ233 <70179486+DannyTZ233@users.noreply.github.com>
2022-04-11 07:47:23 -0500

    Add files via upload

--- Backend/db.py
+++ Backend/db.py
@@ -1,3 +1,53 @@
-from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+from mysql.connector import Error
 
-db = SQLAlchemy()
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def execute_read_query_dict(connection, query):
+    cursor = connection.cursor(dictionary=True)
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+db_conn = create_connection(
+    '127.0.0.1', 'root', 'Alife355005', 'CIS4375db')
