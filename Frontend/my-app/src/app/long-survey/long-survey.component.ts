e5434d5a7823f821732fd8c4703129f7e8bac058
gescobed <gescobedo3@uh.edu>
2022-04-15 16:08:30 -0500

    still in progress

--- Frontend/my-app/src/app/long-survey/long-survey.component.ts
+++ Frontend/my-app/src/app/long-survey/long-survey.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { RatingsService } from '../API/ratings/ratings.service';
+import { ServiceReviewQsService } from '../API/serviceReviewQs/service-review-qs.service';
+import { ServiceReviewsService } from '../API/serviceReviews/service-reviews.service';
+import { EnvReviewQsService } from '../API/envReviewQs/env-review-qs.service';
+import { EnvReviewsService } from '../API/envReviews/env-reviews.service';
+import { DishReviewQsService } from '../API/dishReviewQs/dish-review-qs.service';
+import { DishReviewsService } from '../API/dishReviews/dish-reviews.service';
 
 @Component({
   selector: 'app-long-survey',
