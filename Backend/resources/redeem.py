b1496327da4b0ae89ce8e782e5d97c8b4775f319
DannyTZ233 <70179486+DannyTZ233@users.noreply.github.com>
2022-04-16 10:55:18 -0500

    Add files via upload

--- Backend/resources/redeem.py
+++ Backend/resources/redeem.py
@@ -3,6 +3,17 @@ from db import *
 
 
 class Redeem(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('point',
+                        type=str,
+                        # required=True,
+                        help="required"
+                        )
+    parser.add_argument('redeem_id',
+                        type=str,
+                        # required=True,
+                        help="required"
+                        )
 
     def get(self, point):
         query = f"SELECT * FROM redeem WHERE  point= '{point}'"
@@ -33,6 +44,20 @@ class Redeem(Resource):
         else:
             return {'message': 'record not found'}, 404
         return {'message': 'record deleted'}
+
+    def put(self):
+        data = Redeem.parser.parse_args()
+        query = f"SELECT * FROM redeem WHERE redeem_id = '{data['redeem_id']}'"
+        res = execute_read_query_dict(db_conn, query)
+        if res:
+            update_query = f"""
+                UPDATE redeem
+                SET point = '{data['point']}'
+                WHERE redeem_id = '{data['redeem_id']}'
+            """
+            execute_query(db_conn, update_query)
+            return {"message": "record updated"}, 201
+        return {"message": "record not found"}, 404
 
 
 class RedeemList(Resource):
