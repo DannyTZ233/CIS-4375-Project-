3af000b15d743aca5b4394365669beeddc31031c
Brandon B <45802468+donnerforbreakfast@users.noreply.github.com>
2022-04-30 21:29:23 -0500

    Clock Page for clocking in and out
    Finished clock in and clock out page

--- Frontend/my-app/src/app/employee-timesheet/employee-timesheet.component.ts
+++ Frontend/my-app/src/app/employee-timesheet/employee-timesheet.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { EmployeeTimesheetService } from './employee-timesheet.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-timesheet',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeTimesheetComponent implements OnInit {
 
-  constructor() { }
+
+  empID: any;
+  data:any;
+
+  constructor(private router: Router,
+    private employeeTimesheetService: EmployeeTimesheetService) { }
 
   ngOnInit(): void {
   }
 
+
+
+  clockIn() {
+    
+    console.log(this.data, this.empID);
+    this.employeeTimesheetService.postClockIn(this.data, this.empID)
+
+  }
+  clockOut() {
+    
+    console.log(this.data, this.empID);
+    this.employeeTimesheetService.putClockOut(this.data, this.empID)
+
+  }
 }
